7edddd91cd4f7152134795cd288eed96b96d2439
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-01-11 13:55:52 +0100

    2 commit

--- src/Rama.java
+++ src/Rama.java
@@ -1,5 +1,12 @@
 public class Rama {
     public static void main(String[] args) {
         System.out.println("Hola");
+
+        System.out.println("2 commit");
+
+        int numero1 = 10;
+        int numero2 = 11;
+        int numero3 = 12;
+
     }
 }
